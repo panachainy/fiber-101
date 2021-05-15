4d515c33baf5ecaaba4ed49cf85dbcc0b547d5a8
Panachai <panachai.ny@gmail.com>
2021-05-15 14:56:19 +0700

    add exception handler

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/recover"
 )
 
 type Todo struct {
@@ -17,8 +20,30 @@ var todos = []Todo{
 }
 
 func main() {
-	app := fiber.New()
+	app := fiber.New(fiber.Config{
+		// Override default error handler
+		ErrorHandler: func(ctx *fiber.Ctx, err error) error {
+			// Status code defaults to 500
+			code := fiber.StatusInternalServerError
+
+			// Retrieve the custom status code if it's an fiber.*Error
+			if e, ok := err.(*fiber.Error); ok {
+				code = e.Code
+			}
+
+			// Send custom error page
+			err = ctx.Status(code).SendFile(fmt.Sprintf("./%d.html", code))
+			if err != nil {
+				// In case the SendFile fails
+				return ctx.Status(500).SendString("Internal Server Error")
+			}
+
+			// Return from handler
+			return nil
+		},
+	})
 	app.Use(logger.New())
+	app.Use(recover.New())
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
@@ -26,6 +51,27 @@ func main() {
 
 	app.Get("/version", func(c *fiber.Ctx) error {
 		return c.SendStatus(200)
+	})
+
+	// Example Exception
+
+	app.Get("/1", func(c *fiber.Ctx) error {
+		// Pass error to Fiber
+		return c.SendFile("file-does-not-exist")
+	})
+
+	app.Get("/2", func(c *fiber.Ctx) error {
+		panic("This panic is catched by fiber")
+	})
+
+	app.Get("/3", func(c *fiber.Ctx) error {
+		// 503 Service Unavailable
+		return fiber.ErrServiceUnavailable
+	})
+
+	app.Get("/4", func(c *fiber.Ctx) error {
+		// 503 On vacation!
+		return fiber.NewError(fiber.StatusServiceUnavailable, "On vacation!")
 	})
 
 	app.Listen(":5000")
