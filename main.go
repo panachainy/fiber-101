466dcc5220e64efbf65e16accfdadd489be8b9dc
Panachai <panachai.ny@gmail.com>
2021-05-23 19:11:37 +0700

    add toturial

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fiber-101/mockresponse"
+	"fiber-101/services"
 	"fmt"
 
 	"github.com/gofiber/fiber/v2"
@@ -78,6 +79,102 @@ func main() {
 
 	app.Get("/mock-response", func(c *fiber.Ctx) error {
 		return c.SendString(mockresponse.GetResponse())
+	})
+
+	app.Get("/arrays", func(c *fiber.Ctx) error {
+		langs := [4]string{
+			"golang",
+			"typescript",
+		}
+		fmt.Printf("1) langs: %v \n", langs)
+		fmt.Printf("1) langs: %#v \n", langs)
+
+		// Get value specific array
+		n := langs[1]
+		fmt.Printf("2) langs: %#v \n", n)
+
+		// Set
+		langs[0] = "golang level 2"
+		fmt.Printf("3) langs: %#v \n", langs)
+
+		return c.SendString("see your console")
+	})
+
+	app.Get("/slices", func(c *fiber.Ctx) error {
+		// init
+		langs := []string{"test", "ja"}
+		fmt.Printf("1) slices langs: %#v \n", langs)
+		fmt.Printf("1) slices langs length: %#v \n", len(langs))
+
+		// append
+		langs = append(langs, "C#")
+		fmt.Printf("2) slices langs: %#v \n", langs)
+		fmt.Printf("2) slices langs length: %#v \n", len(langs))
+
+		// append
+		langs = append(langs, "F#", "Python", "Js")
+		fmt.Printf("3) slices langs: %#v \n", langs)
+		fmt.Printf("3) slices langs length: %#v \n", len(langs))
+
+		return c.SendString("see your console")
+	})
+
+	app.Get("/slicing", func(c *fiber.Ctx) error {
+		fmt.Printf("1 slice ============= \n")
+
+		langs := []string{"golang", "java", "js"}
+		newLangs1 := langs[0:2] // get just 0 1
+		fmt.Printf("1) slicing langs: %#v \n", newLangs1)
+		fmt.Printf("1) slicing langs length: %#v \n", langs)
+
+		newLangs2 := langs[1:3] // get just 1 2
+		fmt.Printf("2) slicing langs: %#v \n", newLangs2)
+		fmt.Printf("2) slicing langs old value: %#v \n", langs)
+
+		newLangs3 := langs[1:len(langs)] // get just 1 2
+		fmt.Printf("3) slicing langs: %#v \n", newLangs3)
+		fmt.Printf("3) slicing langs old value: %#v \n", langs)
+
+		a := langs[0:3]
+		fmt.Printf("a) slicing langs: %#v \n", a)
+		b := langs[:3]
+		fmt.Printf("b) slicing langs: %#v \n", b)
+
+		c2 := langs[0:]
+		fmt.Printf("c) slicing langs: %#v \n", c2)
+
+		d := langs[:]
+		fmt.Printf("d) slicing langs: %#v \n", d)
+
+		fmt.Printf("2 array to slice ============= \n")
+
+		services.PrintSlice(langs)
+
+		names := [2]string{"dam", "ping"}
+
+		// services.PrintSlice(names) // Can't do like this
+		services.PrintSlice(names[:])
+
+		fmt.Printf("3 zero slice ============= \n")
+
+		var counties []string
+		fmt.Printf("counties : %#v \n", counties)
+
+		if counties == nil {
+			fmt.Printf("counties is nil \n")
+		} else {
+			fmt.Printf("counties is NOT nil, value:%#v\n", counties)
+		}
+
+		counties2 := []string{"thai"}
+		fmt.Printf("counties : %#v \n", counties2)
+
+		if counties == nil {
+			fmt.Printf("counties2 is nil \n")
+		} else {
+			fmt.Printf("counties2 is NOT nil, value:%#v\n", counties2)
+		}
+		return c.SendString("see your console")
 	})
 
 	app.Listen(":5000")
