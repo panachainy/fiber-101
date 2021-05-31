c0afa64f48e729f7e67e4a5eb95d70d7702ac939
Panachai <panachai.ny@gmail.com>
2021-06-01 00:43:24 +0700

    add CRUD

--- handler/product.go
+++ handler/product.go
@@ -3,19 +3,35 @@ package handler
 import (
 	"fiber-101/database"
 	"fiber-101/model"
+	"fmt"
 
 	"github.com/gofiber/fiber/v2"
 )
 
-func GetAllProducts(c *fiber.Ctx) error {
+func GetProducts(c *fiber.Ctx) error {
 	var product []model.Product
 
 	database.DBConn.Find(&product)
 
 	return c.JSON(fiber.Map{
-		"message": "I'm a GET request!",
-		"data":    product,
+		"data": product,
 	})
+}
+
+func GetProduct(c *fiber.Ctx) error {
+	id := c.Params("id")
+
+	db := database.DBConn
+	var product model.Product
+	db.Find(&product, id)
+	if product.ID == 0 {
+		c.SendStatus(404)
+		return c.JSON(fiber.Map{
+			"message": fmt.Sprintf("Not found product id %v", id),
+		})
+	}
+
+	return c.JSON(product)
 }
 
 func CreateProduct(c *fiber.Ctx) error {
@@ -34,4 +50,23 @@ func CreateProduct(c *fiber.Ctx) error {
 	db := database.DBConn
 	db.Create(&product)
 	return c.SendStatus(201)
+}
+
+func DeleteProduct(c *fiber.Ctx) error {
+	id := c.Params("id")
+	db := database.DBConn
+
+	var product model.Product
+
+	db.First(&product, id)
+
+	if product.ID == 0 {
+		c.SendStatus(404)
+		return c.JSON(fiber.Map{
+			"message": fmt.Sprintf("Not found product id %v", id),
+		})
+	}
+
+	db.Delete(&product)
+	return c.SendStatus(204)
 }
