da4fd77aa3d6564af398436ce849fb7734966d1a
Panachai <panachai.ny@gmail.com>
2021-07-10 19:07:15 +0700

    migrate log to logrus

--- config/config.go
+++ config/config.go
@@ -1,16 +1,16 @@
 package config
 
 import (
-	"log"
 	"os"
 
 	"github.com/joho/godotenv"
+	"github.com/sirupsen/logrus"
 )
 
 func Get(key string) string {
 	err := godotenv.Load(".env")
 	if err != nil {
-		log.Println("Error loading .env file:", err)
+		logrus.Errorln("Error loading .env file:", err)
 	}
 
 	return os.Getenv(key)
