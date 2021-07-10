cc85ff1abd163cd9852d864ba51f0a7d8d6b9601
Panachai <panachai.ny@gmail.com>
2021-07-10 17:19:54 +0700

    [GORM] update DATABASE_DSN way to connect db

--- database/database.go
+++ database/database.go
@@ -4,6 +4,7 @@ import (
 	"fiber-101/config"
 	"fiber-101/model"
 	"fmt"
+	"log"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -13,16 +14,22 @@ var DBConn *gorm.DB
 
 func Init() {
 	var err error
+	var dsn string
 
-	dsn := fmt.Sprintf("host=%v user=%v password=%v dbname=%v port=%v sslmode=%v TimeZone=%v",
-		config.Get("DB_HOST"),
-		config.Get("DB_USER"),
-		config.Get("DB_PASSWORD"),
-		config.Get("DB_NAME"),
-		config.Get("DB_PORT"),
-		config.Get("DB_SSLMODE"),
-		config.Get("DB_TIMEZONE"))
-
+	if config.Get("DATABASE_DSN") != "" {
+		dsn = config.Get("DATABASE_DSN")
+		log.Println("Use config DATABASE_DSN")
+	} else {
+		dsn = fmt.Sprintf("host=%v user=%v password=%v dbname=%v port=%v sslmode=%v TimeZone=%v",
+			config.Get("DB_HOST"),
+			config.Get("DB_USER"),
+			config.Get("DB_PASSWORD"),
+			config.Get("DB_NAME"),
+			config.Get("DB_PORT"),
+			config.Get("DB_SSLMODE"),
+			config.Get("DB_TIMEZONE"))
+		log.Println("Use split config DATABASE")
+	}
 	DBConn, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("Failed to connect database")
