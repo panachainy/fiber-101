4e52388c2056460db4a91df8452168eae6c8cdb6
Panachai <panachai.ny@gmail.com>
2021-07-10 23:21:42 +0700

    change to debug log

--- config/config.go
+++ config/config.go
@@ -4,12 +4,13 @@ import (
 	"os"
 
 	"github.com/joho/godotenv"
+	"github.com/sirupsen/logrus"
 )
 
 func Get(key string) string {
 	err := godotenv.Load(".env")
 	if err != nil {
-		// logrus.Errorln("Error loading .env file:", err)
+		logrus.Debugln("[CONFIG] Error loading .env file:", err)
 	}
 
 	return os.Getenv(key)
