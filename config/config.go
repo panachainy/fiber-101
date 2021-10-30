3365829949b1c94cb28ced2f83a0aa74d3931c59
Panachai <panachai.ny@gmail.com>
2021-10-30 18:23:22 +0700

    add TestGetConfig and refactor for testable

--- config/config.go
+++ config/config.go
@@ -7,8 +7,14 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-func Get(key string) string {
-	err := godotenv.Load(".env")
+func Get(key string, envPath ...string) string {
+	path := ".env"
+
+	if len(envPath) > 0 {
+		path = envPath[0]
+	}
+
+	err := godotenv.Load(path)
 	if err != nil {
 		logrus.Debugln("[CONFIG] Error loading .env file:", err)
 	}
